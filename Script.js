c1b854eca0e09dacf87ccc05c4bcd27b0de8db14
rodolfo <rodolfosouza595@gmail.com>
2025-04-19 12:31:53 -0300

    commit-projeto-mario

---
+++ Script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao() {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+ 
